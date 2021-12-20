ef79959e122c20c1702af2e7a57bf7c679c2f62a
binaymalla <binaymalla76@gmail.com>
2021-12-19 23:51:34 -0800

    day2

---
+++ day2/lab1.py
@@ -0,0 +1,16 @@
+#Convert seconds to day,hours,minutes and seconds
+sec = 86400 
+Day = sec/86400
+hour = sec/(60*60)
+min =sec/(60)
+print(Day)
+print (hour)
+print (min)
+
+sec = int (input ("enter time in seconds"))
+Day = sec/86400
+hour = sec/(60*60)
+min =sec/(60)
+print(Day)
+print (hour)
+print (min)
