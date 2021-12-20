cfaf453c5c67f194bff1fc68dd87eea4da4167ee
binaymalla <binaymalla76@gmail.com>
2021-12-19 23:16:50 -0800

    day1

---
+++ day1/lab2.py
@@ -0,0 +1,5 @@
+#Area Of Triangle
+b =int (input ("enter the breadth of a triangle:"))
+h =int (input ("enter the height if a triangle:"))
+area = 1/2*(b*h)
+print ("The area of triangle is :", area,"cm\u00b2".format(area))
