8bae996f49d08634b5708aa91a0347b0b6680f63
binaymalla <binaymalla76@gmail.com>
2021-12-20 00:01:40 -0800

    day2

---
+++ day2/lab4.py
@@ -0,0 +1,5 @@
+k = int(input ("Enter the no of apples in basket"))
+N = int (input ("Enter the no of students"))
+rem = k//N
+bas = k%N
+print ("Each students gets",rem, "apples" "and""The rem apples on basket are ",bas)
