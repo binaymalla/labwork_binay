7834ce4f876d303890227c3c25f4e386a2196d04
binaymalla <binaymalla76@gmail.com>
2021-12-19 23:26:36 -0800

    day1

---
+++ day1/lab3.py
@@ -0,0 +1,6 @@
+# volume of Cuboid
+l= int (input ("enter the length of the cuboid:"))
+b = int (input ("enter the breadth of the cuboid:"))
+h = int (input("enter the height of cuboid:"))
+vol = l*b*h
+print (f"the volume of cuboid is:",vol,"cm\u00b3")
