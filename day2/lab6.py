f61a70df50bb1c258a29630ad1cca9c22fd61e10
binaymalla <binaymalla76@gmail.com>
2021-12-20 00:05:56 -0800

    day2

---
+++ day2/lab6.py
@@ -0,0 +1,8 @@
+bus = (4/(25/60)+2*10)
+print ("minutes for bus is ",bus)
+walk = (1/(7/60)+2/(15/60)+1/(7/60))
+print ("minutes for walk is ",walk)
+if bus>walk:
+    print ("bus is quicker")
+else:
+    print("walk is quicker")
