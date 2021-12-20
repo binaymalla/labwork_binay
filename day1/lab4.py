2fb9502856da60c8861e841c80deaff59f5207d8
binaymalla <binaymalla76@gmail.com>
2021-12-19 23:33:09 -0800

    day1

---
+++ day1/lab4.py
@@ -0,0 +1,12 @@
+# sum of first 10 positive number
+n = 10
+sumn= (n * (n + 1)) / 2
+print("Sum of the first", 10 ,"positive integers:", sumn)
+
+a = range (1, 11)
+sumn = sum(a)
+print(sumn)
+
+listnum ={1,2,3,4,5,6,7,8,9,10}
+sumB =sum(listnum)
+print(sumB)
