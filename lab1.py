46cb8c6c1477c35c760ee17e880248d1dc56aff6
binaymalla <binaymalla76@gmail.com>
2021-12-19 23:03:49 -0800

    day1

---
+++ lab1.py
@@ -0,0 +1,6 @@
+# sum of three numbers
+a = input ("enter first number:\n")
+b = input("enter 2nd number:\n")
+c = input("enter 3rd number:\n")
+d = int(a)+int(b)+int(c) 
+print ("the sum of the number is :",d)
