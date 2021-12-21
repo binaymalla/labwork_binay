63ba79e33757d7b15002aba520692e242c674150
binaymalla <binaymalla76@gmail.com>
2021-12-21 04:03:06 -0800

    day2

--- day2/lab6.py
+++ day2/lab6.py
@@ -1,8 +1,8 @@
 bus = (4/(25/60)+2*10)
 print ("minutes for bus is ",bus)
-walk = (1/(7/60)+2/(15/60)+1/(7/60))
-print ("minutes for walk is ",walk)
-if bus>walk:
+wal = (1/(7/60)+2/(15/60)+1/(7/60))
+print ("minutes for walk is ",wal)
+if bus>wal:
     print ("bus is quicker")
 else:
     print("walk is quicker")
--- day3/lab1.py
+++
@@ -1,6 +0,0 @@
-#whether 5 is in list of first five integer or not 
-l=(1,2,3,4,5)
-if 5 in l:
-    print ("yes, 5 is in ln")
-else:
-    print ("no,5 is not in ln") 
