d04e93d423e5ce34f9bec81ed6aef9b2257dcd4b
binaymalla <binaymalla76@gmail.com>
2021-12-21 04:13:31 -0800

    day3

---
+++ day3/lab5.py
@@ -0,0 +1,8 @@
+# 5. For given integer x, print ‘True’ if it is positive, print ‘False’ if it is negative and print ‘zero’ if it is 0.
+a = int (input ("Enter a num "))
+if a % 2 == 0:
+    print("True")
+elif a % 2 != 0:
+    print ("false")
+else:
+    print ("zero")
