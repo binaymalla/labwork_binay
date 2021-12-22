704e0543aa2b63c063b34f92ce8b8d99e6e66d6b
binaymalla <binaymalla76@gmail.com>
2021-12-22 02:31:27 -0800

    day5

---
+++ day5/lab4.py
@@ -0,0 +1,16 @@
+# Write a Python program to construct the following pattern, using a nested for loop.
+# *
+# **
+# ***
+# ****
+# ***
+# **
+# *
+for i in range(0, 4):
+    for j in range(0, i + 1):
+        print("*",end='')
+    print ("")
+for i in range (5,0,-1):
+    for j  in range (0, i):
+        print("*",end='')
+    print ('')
