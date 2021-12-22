847fa25ce627a6f291b4c0da0c9c47623bf30384
binaymalla <binaymalla76@gmail.com>
2021-12-22 02:42:55 -0800

    day5

---
+++ day5/lab9.py
@@ -0,0 +1,12 @@
+# Write a program to find the factorial of a number.
+
+num = int(input("Enter the Number :"))
+ 
+fact = 1
+ 
+if num < 0:
+    print("Factorial of negative number is not defined")
+else:
+    for i in range(1,num+1):
+        fact *= i
+    print("Factorial of {} is {}".format(num, fact))
